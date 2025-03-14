3d2689509367109c689ea919e3c0798de7710b85
Spectre09 <jvargas@broditec.net>
2025-03-13 22:16:54 -0600

    feat: refactor naming convention transformation functions

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,7 @@
 // VSCode
 import * as vscode from "vscode";
-
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+// Enums
+import { NamingConvention } from "./enums/naming-convention.enum";
 
 // Valid scenaries
 // - No active text editor
@@ -21,9 +20,6 @@ import * as vscode from "vscode";
 // Priorize separations characters
 // (white space) - _ . ...el _ si es el primer character no tomarlo en cuenta, el orden se basa en este orden
 // Si se encuentra otro characters que no sea letra, número o separador, remplace por un vacío
-
-// TODO refactor, and improve documentation
-
 const getSelectionStatement = (): vscode.Selection | null => {
   const editor = vscode.window.activeTextEditor;
 
@@ -70,258 +66,169 @@ const getWords = (text: string): string[] => {
     return formattedText.split(".");
   }
 
+  if (/^[A-Z]+$/.test(formattedText)) {
+    return [formattedText];
+  }
+
   return formattedText.split(/(?=[A-Z])/);
 };
 
-const transformToCamelCase = (text: string): string => {
+const transformToConvention = ({
+  text,
+  separator = "",
+  wordTransformer,
+  replace,
+}: {
+  text: string;
+  wordTransformer: (word: string, index: number) => string;
+  separator: string;
+  replace: RegExp | string;
+}): string => {
   const words = getWords(text);
 
   if (!words.length) {
     return "";
   }
 
-  return words
-    .map((word, index) => {
-      if (index === 0) {
-        return word.toLowerCase();
-      }
+  return words.map(wordTransformer).join(separator).replace(replace, "");
+};
 
-      const firstCharacter = word.charAt(0).toUpperCase();
-      const restOfWord = word.slice(1).toLowerCase();
+const transformToCamelCase = (text: string): string => {
+  const callback = (word: string, index: number) => {
+    if (index === 0) {
+      return word.toLowerCase();
+    }
 
-      return `${firstCharacter}${restOfWord}`;
-    })
-    .join("")
-    .replace(/(?!^)_|[^a-zA-Z0-9]/g, "");
+    const firstCharacter = word.charAt(0).toUpperCase();
+    const restOfWord = word.slice(1).toLowerCase();
+
+    return `${firstCharacter}${restOfWord}`;
+  };
+
+  return transformToConvention({
+    text,
+    separator: "",
+    wordTransformer: callback,
+    replace: /(?!^)_|[^a-zA-Z0-9]/g,
+  });
 };
 
 const transformToPascalCase = (text: string): string => {
-  const words = getWords(text);
+  const callback = (word: string, index: number) => {
+    if (!index) {
+      return word.toUpperCase();
+    }
 
-  if (!words.length) {
-    return "";
-  }
+    const firstCharacter = word.charAt(0).toUpperCase();
+    const restOfWord = word.slice(1).toUpperCase();
 
-  return words
-    .map((word, index) => {
-      if (index === 0) {
-        return word.toUpperCase();
-      }
+    return `${firstCharacter}${restOfWord}`;
+  };
 
-      const firstCharacter = word.charAt(0).toUpperCase();
-      const restOfWord = word.slice(1).toUpperCase();
-
-      return `${firstCharacter}${restOfWord}`;
-    })
-    .join("")
-    .replace(/(?!^)_|[^a-zA-Z0-9]/g, "");
+  return transformToConvention({
+    text,
+    separator: "",
+    wordTransformer: callback,
+    replace: /(?!^)_|[^a-zA-Z0-9]/g,
+  });
 };
 
 const transformToSnakeCase = (text: string): string => {
-  const words = getWords(text);
+  const callback = (word: string) => word.toLowerCase();
 
-  if (!words.length) {
-    return "";
-  }
-
-  return words
-    .map((word) => {
-      return word.toLowerCase();
-    })
-    .join("_")
-    .replace(/[^a-zA-Z0-9]/g, "");
+  return transformToConvention({
+    text,
+    separator: "_",
+    wordTransformer: callback,
+    replace: /[^a-zA-Z0-9_]/g,
+  });
 };
 
 const transformToScreamingSnakeCase = (text: string): string => {
-  const words = getWords(text);
+  const callback = (word: string) => word.toUpperCase();
 
-  if (!words.length) {
-    return "";
-  }
-
-  return words
-    .map((word) => {
-      return word.toUpperCase();
-    })
-    .join("_")
-    .replace(/[^a-zA-Z0-9]/g, "");
+  return transformToConvention({
+    text,
+    separator: "_",
+    wordTransformer: callback,
+    replace: /[^a-zA-Z0-9_]/g,
+  });
 };
 
 const transformToKebabCase = (text: string): string => {
-  const words = getWords(text);
+  const callback = (word: string) => word.toLowerCase();
 
-  if (!words.length) {
-    return "";
+  return transformToConvention({
+    text,
+    separator: "-",
+    wordTransformer: callback,
+    replace: /[^a-zA-Z0-9]/g,
+  });
+};
+
+const mapHandlerConvention: Readonly<
+  Record<NamingConvention, (value: string) => string>
+> = {
+  [NamingConvention.CAMEL_CASE]: transformToCamelCase,
+  [NamingConvention.PASCAL_CASE]: transformToPascalCase,
+  [NamingConvention.SNAKE_CASE]: transformToSnakeCase,
+  [NamingConvention.SCREAMING_SNAKE_CASE]: transformToScreamingSnakeCase,
+  [NamingConvention.KEBAB_CASE]: transformToKebabCase,
+};
+
+const handleTextTransformation = (convention: NamingConvention) => {
+  const selection = getSelectionStatement();
+
+  if (!selection) {
+    return;
   }
 
-  return words
-    .map((word) => {
-      return word.toLowerCase();
-    })
-    .join("-")
-    .replace(/[^a-zA-Z0-9]/g, "");
+  const selectionText =
+    vscode.window.activeTextEditor?.document.getText(selection);
+
+  if (!selectionText) {
+    return;
+  }
+
+  const handler = mapHandlerConvention[convention];
+
+  if (!handler) {
+    vscode.window.showErrorMessage("Transformation not supported");
+    return;
+  }
+
+  const transformedText = handler(selectionText);
+
+  vscode.window.activeTextEditor?.edit((editBuilder) => {
+    editBuilder.replace(selection, transformedText);
+  });
 };
 
 export function activate(context: vscode.ExtensionContext) {
   const transformToCamelCaseCommand = vscode.commands.registerCommand(
     "naming-conventions.camelCase",
-    () => {
-      vscode.window.showInformationMessage("Transform to camelCase");
-
-      const selection = getSelectionStatement();
-
-      if (!selection) {
-        return;
-      }
-
-      const selectionText =
-        vscode.window.activeTextEditor?.document.getText(selection);
-
-      if (!selectionText) {
-        return;
-      }
-
-      const transformedText = transformToCamelCase(selectionText);
-
-      vscode.window.activeTextEditor?.edit((editBuilder) => {
-        editBuilder.replace(selection, transformedText);
-      });
-    }
+    () => handleTextTransformation(NamingConvention.CAMEL_CASE)
   );
 
   const transformToPascalCaseCommand = vscode.commands.registerCommand(
     "naming-conventions.pascalCase",
-    () => {
-      vscode.window.showInformationMessage("Transform to pascalCase");
-
-      const selection = getSelectionStatement();
-
-      if (!selection) {
-        return;
-      }
-
-      const selectionText =
-        vscode.window.activeTextEditor?.document.getText(selection);
-
-      if (!selectionText) {
-        return;
-      }
-
-      const transformedText = transformToPascalCase(selectionText);
-
-      vscode.window.activeTextEditor?.edit((editBuilder) => {
-        editBuilder.replace(selection, transformedText);
-      });
-    }
+    () => handleTextTransformation(NamingConvention.PASCAL_CASE)
   );
 
   const transformToSnakeCaseCommand = vscode.commands.registerCommand(
     "naming-conventions.snakeCase",
-    () => {
-      vscode.window.showInformationMessage("Transform to snakeCase");
-
-      const selection = getSelectionStatement();
-
-      if (!selection) {
-        return;
-      }
-
-      const selectionText =
-        vscode.window.activeTextEditor?.document.getText(selection);
-
-      if (!selectionText) {
-        return;
-      }
-
-      const transformedText = transformToSnakeCase(selectionText);
-
-      vscode.window.activeTextEditor?.edit((editBuilder) => {
-        editBuilder.replace(selection, transformedText);
-      });
-    }
+    () => handleTextTransformation(NamingConvention.SNAKE_CASE)
   );
 
   const transformToScreamingSnakeCaseCommand = vscode.commands.registerCommand(
     "naming-conventions.screamingSnakeCase",
-    () => {
-      vscode.window.showInformationMessage("Transform to SCREAMING_SNAKE_CASE");
-
-      const selection = getSelectionStatement();
-
-      if (!selection) {
-        return;
-      }
-
-      const selectionText =
-        vscode.window.activeTextEditor?.document.getText(selection);
-
-      if (!selectionText) {
-        return;
-      }
-
-      const transformedText = transformToScreamingSnakeCase(selectionText);
-
-      vscode.window.activeTextEditor?.edit((editBuilder) => {
-        editBuilder.replace(selection, transformedText);
-      });
-    }
+    () => handleTextTransformation(NamingConvention.SCREAMING_SNAKE_CASE)
   );
 
   const transformToKebabCaseCommand = vscode.commands.registerCommand(
     "naming-conventions.kebabCase",
-    () => {
-      vscode.window.showInformationMessage("Transform to kebabCase");
-
-      const selection = getSelectionStatement();
-
-      if (!selection) {
-        return;
-      }
-
-      const selectionText =
-        vscode.window.activeTextEditor?.document.getText(selection);
-
-      if (!selectionText) {
-        return;
-      }
-
-      const transformedText = transformToKebabCase(selectionText);
-
-      vscode.window.activeTextEditor?.edit((editBuilder) => {
-        editBuilder.replace(selection, transformedText);
-      });
-    }
+    () => handleTextTransformation(NamingConvention.KEBAB_CASE)
   );
-
-  //   const transformToDotCaseCommand = vscode.commands.registerCommand(
-  //     "naming-conventions.dotCase",
-  //     () => {
-  //       vscode.window.showInformationMessage("Transform to dotCase");
-
-  //       const selection = getSelectionStatement();
-
-  //       if (!selection) {
-  //         return;
-  //       }
-
-  //       console.log({ selection });
-  //     }
-  //   );
-
-  //   const transformToTrainCaseCommand = vscode.commands.registerCommand(
-  //     "naming-conventions.trainCase",
-  //     () => {
-  //       vscode.window.showInformationMessage("Transform to trainCase");
-
-  //       const selection = getSelectionStatement();
-
-  //       if (!selection) {
-  //         return;
-  //       }
-
-  //       console.log({ selection });
-  //     }
-  //   );
 
   context.subscriptions.push(
     transformToCamelCaseCommand,
@@ -329,7 +236,5 @@ export function activate(context: vscode.ExtensionContext) {
     transformToSnakeCaseCommand,
     transformToScreamingSnakeCaseCommand,
     transformToKebabCaseCommand
-    // transformToDotCaseCommand,
-    // transformToTrainCaseCommand
   );
 }
